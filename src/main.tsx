535ea55d24760f828ff832848335ae3f26428880
rreno1 <rrenomeronjr@gmail.com>
2026-08-22 16:33:31 +0800

    Use consolidated app stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import '../css/tokens.css';
-import '../css/base.css';
-import '../css/components.css';
-import '../css/pages.css';
-import '../css/responsive.css';
-import '../css/typography.css';
+import './styles/app.css';
 import { App } from './App';
 import { captureAttribution } from './lib/nj125Api';
 import { PropertyProvider } from './state/PropertyContext';
@@ -14,7 +9,7 @@ import { PropertyProvider } from './state/PropertyContext';
 captureAttribution();
 
 const root = document.getElementById('root');
-if (!root) throw new Error('RenoLeads root element is missing');
+if (!root) throw new Error('renoleads root element is missing');
 
 createRoot(root).render(
   <StrictMode>
