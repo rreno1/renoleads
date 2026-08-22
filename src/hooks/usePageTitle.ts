8248a6d2d32b5b05565117091e92d4f0c395df87
rreno1 <rrenomeronjr@gmail.com>
2026-08-22 16:20:00 +0800

    Normalize RenoLeads page title branding

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -2,6 +2,6 @@ import { useEffect } from 'react';
 
 export function usePageTitle(title: string) {
   useEffect(() => {
-    document.title = title;
+    document.title = title.replaceAll('RenoLeads', 'renoleads');
   }, [title]);
 }
