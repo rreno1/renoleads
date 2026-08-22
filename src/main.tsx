27b393fe83b596520332b888f39288ad9a4c8756
rreno1 <rrenomeronjr@gmail.com>
2026-08-22 16:35:58 +0800

    Load refined form styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/app.css';
+import './styles/forms.css';
 import { App } from './App';
 import { captureAttribution } from './lib/nj125Api';
 import { PropertyProvider } from './state/PropertyContext';
